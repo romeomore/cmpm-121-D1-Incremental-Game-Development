0e4c63bcfeef65f1c4fe802bf206128258a85e00
rmoren15@ucsc.edu <rmoren15@ucsc.edu>
2025-10-08 08:16:24 +0000

    Changed main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Hello world! I just edited this page.</p>
+  <p>Want to build a clicker that works around chickens that evolve and drop better eggs as you upgrade.</p>
 `;
