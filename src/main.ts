9dd61552f1860c5084c7b76b42b726260224dce5
rmoren15@ucsc.edu <rmoren15@ucsc.edu>
2025-10-08 08:42:11 +0000

    Added a clickable button that updates egg count.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
+let count = 0;
+
 document.body.innerHTML = `
-  
   <p>Hello world! I just edited this page.</p>
   <p>Want to build a clicker that works around chickens that evolve and drop better eggs as you upgrade.</p>
 `;
+
+const button = document.createElement("button");
+button.textContent = "Click Me To Gain An Egg!!";
+document.body.appendChild(button);
+button.addEventListener("click", () => {
+  count++;
+  button.textContent = `You Have ${count} Eggs`;
+});
