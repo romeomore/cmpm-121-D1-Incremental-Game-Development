1ee0fc2978dc1f1af5a141fea8574b930b904e71
rmoren15@ucsc.edu <rmoren15@ucsc.edu>
2025-10-08 17:00:02 +0000

    Co-authored-by: Adira Rana <AlphaAdira@users.noreply.github.com> Co-authored-by: Fishbowl <TrueAppleOperation@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ let count = 0;
 
 document.body.innerHTML = `
   <p>Hello world! I just edited this page.</p>
+  <p></p>
   <p>Want to build a clicker that works around chickens that evolve and drop better eggs as you upgrade.</p>
+  <p>Hello World, I'm an egg counter</p> 
 `;
 
 const button = document.createElement("button");
@@ -13,4 +15,11 @@ document.body.appendChild(button);
 button.addEventListener("click", () => {
   count++;
   button.textContent = `You Have ${count} Eggs`;
+
+  setInterval(autoClicker, 1000);
+
+  function autoClicker() {
+    count++;
+    button.textContent = `You Have ${count} Eggs`;
+  }
 });
