cda73ab7735af7c3180bcdef802c6d902bc10945
rmoren15@ucsc.edu <rmoren15@ucsc.edu>
2025-10-08 08:21:28 +0000

    Made changes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  
   <p>Hello world! I just edited this page.</p>
   <p>Want to build a clicker that works around chickens that evolve and drop better eggs as you upgrade.</p>
 `;
